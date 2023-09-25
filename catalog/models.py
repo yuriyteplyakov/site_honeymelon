f4a27d174a424444e0831d31f830931256d860c4
Yuriy Teplyakov <teplyakov_1997@inbox.ru>
2023-09-25 21:30:50 +0500

    готовый каталог без деталей

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from django.utils.text import slugify
 # Create your models here.
 
 class Category(models.Model):
@@ -11,6 +11,11 @@ class Category(models.Model):
         ordering = ('name',)
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -23,7 +28,7 @@ class Product(models.Model):
     name = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(max_length=200, blank=True, null=True, help_text="не более 200 символов")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
@@ -35,6 +40,11 @@ class Product(models.Model):
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
         index_together = (('id', 'slug'),)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
  
     def __str__(self):
         return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -8,11 +8,11 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'catalog/product/list-catalog.html',
+    return render(request, 'catalog/product/list.html',
                   {'category': category, 'categories': categories,
                    'products': products})
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'catalog/product/detail-catalog.html', {'product': product})
+    return render(request, 'catalog/product/detail.html', {'product': product})
 
